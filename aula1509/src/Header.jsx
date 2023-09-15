b34cf3fa1a0013dfa7295b50888f80d53f429828
Ester Bendicto <esterbendicto@gmail.com>
2023-09-15 15:41:11 -0300

    commit 01

---
+++ aula1509/src/Header.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Header(){
+    return (
+        <>
+        <header>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>Sobre</li>
+                    <li>Contatos</li>
+                    <li>Endereços</li>
+                </ul>
+            </nav>
+        </header>
+        </>
+    )
+}
+export default Header
